9fb608b6ab1ddb7fd7baaebbed3df1e407ce563b
Mitchell Barry <mitch.barry@gmail.com>
2020-12-11 21:04:48 -0600

    Day 8-10 solutions

--- AdventOfCode/Solutions/ASolution.cs
+++ AdventOfCode/Solutions/ASolution.cs
@@ -125,7 +125,7 @@ namespace AdventOfCode.Solutions
             {
                 return solver();
             }
-            catch( Exception ) {
+            catch( Exception ex ) {
                 if( Debugger.IsAttached )
                 {
                     Debugger.Break();
--- AdventOfCode/Solutions/Utilities.cs
+++ AdventOfCode/Solutions/Utilities.cs
@@ -172,5 +172,10 @@ namespace AdventOfCode.Solutions
                 hashSet.Add(item);
             }
         }
+
+        public static List<T> Clone<T>(this IList<T> listToClone) where T: ICloneable
+        {
+            return listToClone.Select(item => (T)item.Clone()).ToList();
+        }
     }
 }
