8b92351a54876bcbc31e93491452d74e5a3c38ba
Mitchell Barry <mitch.barry@gmail.com>
2025-11-30 23:43:40 -0600

    Not quite right Day 1

--- AdventOfCode.Solutions/Year2025/Day01/Solution.cs
+++ AdventOfCode.Solutions/Year2025/Day01/Solution.cs
@@ -6,11 +6,65 @@ class Solution : SolutionBase
 
     protected override string? SolvePartOne()
     {
-        return null;
+        var startPosition = 50;
+        var timesAtZero = 0;
+        foreach (var line in Input.SplitByNewline())
+        {
+            var direction = line[0];
+            var rotations = int.Parse(line[1..]);
+            switch (direction)
+            {
+                case 'L':
+                    startPosition += rotations;
+                    break;
+                case 'R':
+                    startPosition -= rotations;
+                    break;
+                default:
+                    throw new Exception("huh?");
+            }
+            startPosition = (startPosition + 100) % 100;
+            if (startPosition == 0)
+            {
+                timesAtZero++;
+            }
+            
+        }
+        return timesAtZero.ToString();
     }
 
     protected override string? SolvePartTwo()
     {
-        return null;
+        var position = 50;
+        var timesAtZero = 0;
+
+        var lastPosition = position;
+        foreach (var line in Input.SplitByNewline())
+        {
+            var direction = line[0];
+            var rotations = int.Parse(line[1..]);
+            switch (direction)
+            {
+                case 'L':
+                    position += rotations;
+                    break;
+                case 'R':
+                    position -= rotations;
+                    break;
+                default:
+                    throw new Exception("huh?");
+            }
+            if (position < 0 || position >= 100)
+            {
+                var fullRotations = (int)Math.Round(position / 100f, MidpointRounding.ToZero);
+                timesAtZero += Math.Abs(fullRotations);
+            }
+            position = (position + 100) % 100;
+            if (position == 0)
+            {
+                timesAtZero++;
+            }
+        }
+        return timesAtZero.ToString();
     }
 }
