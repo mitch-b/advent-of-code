9b90e8b63d8f51a9023fbe62964e15a3c5c1bcea
Mitchell Barry <mitch.barry@gmail.com>
2025-12-01 19:00:12 +0000

    Breakout logic, fix Day 1 Part 2

--- AdventOfCode.Solutions/Year2025/Day01/Solution.cs
+++ AdventOfCode.Solutions/Year2025/Day01/Solution.cs
@@ -2,29 +2,21 @@ namespace AdventOfCode.Solutions.Year2025.Day01;
 
 class Solution : SolutionBase
 {
-    public Solution() : base(01, 2025, "", useDebugInput: true) { }
+    public Solution() : base(01, 2025, "", useDebugInput: false) { }
 
     protected override string? SolvePartOne()
     {
-        var startPosition = 50;
+        var position = 50;
         var timesAtZero = 0;
         foreach (var line in Input.SplitByNewline())
         {
             var direction = line[0];
             var rotations = int.Parse(line[1..]);
-            switch (direction)
-            {
-                case 'L':
-                    startPosition += rotations;
-                    break;
-                case 'R':
-                    startPosition -= rotations;
-                    break;
-                default:
-                    throw new Exception("huh?");
-            }
-            startPosition = (startPosition + 100) % 100;
-            if (startPosition == 0)
+            position += direction == 'L' 
+                ? -rotations 
+                : rotations;
+            position = NormalizePosition(position);
+            if (position == 0)
             {
                 timesAtZero++;
             }
@@ -38,33 +30,61 @@ class Solution : SolutionBase
         var position = 50;
         var timesAtZero = 0;
 
-        var lastPosition = position;
         foreach (var line in Input.SplitByNewline())
         {
             var direction = line[0];
             var rotations = int.Parse(line[1..]);
-            switch (direction)
-            {
-                case 'L':
-                    position += rotations;
-                    break;
-                case 'R':
-                    position -= rotations;
-                    break;
-                default:
-                    throw new Exception("huh?");
-            }
-            if (position < 0 || position >= 100)
-            {
-                var fullRotations = (int)Math.Round(position / 100f, MidpointRounding.ToZero);
-                timesAtZero += Math.Abs(fullRotations);
-            }
-            position = (position + 100) % 100;
-            if (position == 0)
-            {
-                timesAtZero++;
-            }
+
+            var previousPosition = position;
+            position += direction == 'L' 
+                ? -rotations 
+                : rotations;
+
+            var newPosition = NormalizePosition(position);
+            var zeroesHit = CountZeroCrossings(previousPosition, rotations, direction);
+
+            timesAtZero += zeroesHit;
+
+            Console.WriteLine($"From: {previousPosition} to {newPosition}, rotated {direction} {rotations} times. Touched zero {zeroesHit} times.");
+
+            position = newPosition;
         }
+        
         return timesAtZero.ToString();
+    }
+
+    private int NormalizePosition(int position)
+    {
+        if (position >= 0)
+        {
+            return position % 100;
+        }
+        var mod = Math.Abs(position) % 100;
+        return mod == 0 
+            ? 0 
+            : 100 - mod;
+    }
+
+    private int CountZeroCrossings(int startPosition, int rotations, char direction)
+    {
+        if (startPosition == 0)
+        {
+            return rotations / 100;
+        }
+        
+        var fullRotations = rotations / 100;
+        var remainder = rotations % 100;
+        var zeroesHit = fullRotations;
+
+        if (direction == 'L' && remainder >= startPosition)
+        {
+            zeroesHit++;
+        }
+        else if (direction == 'R' && startPosition + remainder >= 100)
+        {
+            zeroesHit++;
+        }
+
+        return zeroesHit;
     }
 }
